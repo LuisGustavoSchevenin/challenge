c4c693fb88dc0bd687900e57afeaf4bd902c2190
schevenin <gustavo.schevenin@gmail.com>
2024-12-16 19:57:03 -0300

    1 - Defining the domain

---
+++ src/main/java/br/com/challenge/domain/User.java
@@ -0,0 +1,53 @@
+package br.com.challenge.domain;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String country;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
