fa55b403b51b2eac7fec6962d45646acf2c24c30
schevenin <gustavo.schevenin@gmail.com>
2024-12-18 02:15:20 -0300

    3 (2) - Adding tracing data

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,12 @@ repositories {
 
 dependencies {
     // SPRING
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // DATABASE
     runtimeOnly("com.h2database:h2")
@@ -31,8 +33,10 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
-    // LOG
+    // LOG & TRACING
     implementation("org.apache.logging.log4j:log4j-core:2.24.2")
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
 
     // TEST
     testImplementation("org.springframework.boot:spring-boot-starter-test")
